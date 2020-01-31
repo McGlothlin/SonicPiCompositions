21f893ff4c34f201ce83e24bbd8efa2647880ee0
Sean McGlothlin <sean.michael.mcglothlin@gmail.com>
2020-01-31 05:09:21 +0000

    Great Fairy Fountain from Legend of Zelda - right hand arpeggios done, left hand in progress

---
+++ ZeldaFairyFountain.rb
@@ -0,0 +1,317 @@
+# Welcome to Sonic Pi v3.1
+
+use_bpm 80
+
+loop do
+  
+  play :A6
+  play :Bb2, sustain: 0.5
+  sleep 0.25
+  play :D6
+  play :Bb3
+  sleep 0.25
+  play :Bb5
+  play :D4
+  sleep 0.25
+  play :G5
+  play :A4, sustain: 0.5
+  sleep 0.25
+  
+  play :G6
+  sleep 0.25
+  play :D6
+  sleep 0.25
+  play :Bb5
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  
+  play :Fs6
+  sleep 0.25
+  play :D6
+  sleep 0.25
+  play :Bb5
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  
+  play :G6
+  sleep 0.25
+  play :D6
+  sleep 0.25
+  play :Bb5
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  
+  
+  play :G6
+  play :A2, sustain: 0.5
+  sleep 0.25
+  play :C6
+  play :A3
+  sleep 0.25
+  play :A5
+  play :C4
+  sleep 0.25
+  play :F5
+  play :G4, sustain: 0.5
+  sleep 0.25
+  
+  play :F6
+  sleep 0.25
+  play :C6
+  sleep 0.25
+  play :A5
+  sleep 0.25
+  play :F5
+  sleep 0.25
+  
+  play :E6
+  sleep 0.25
+  play :C6
+  sleep 0.25
+  play :A5
+  sleep 0.25
+  play :F5
+  sleep 0.25
+  
+  play :F6
+  sleep 0.25
+  play :C6
+  sleep 0.25
+  play :A5
+  sleep 0.25
+  play :F5
+  sleep 0.25
+  
+  
+  play :F6
+  play :G2, sustain: 0.5
+  sleep 0.25
+  play :Bb5
+  play :G3
+  sleep 0.25
+  play :G5
+  play :Bb3
+  sleep 0.25
+  play :E5
+  play :F4, sustain: 0.5
+  sleep 0.25
+  
+  play :E6
+  sleep 0.25
+  play :Bb5
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  play :E5
+  sleep 0.25
+  
+  play :Eb6
+  sleep 0.25
+  play :Bb5
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  play :E5
+  sleep 0.25
+  
+  play :E6
+  sleep 0.25
+  play :Bb5
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  play :E5
+  sleep 0.25
+  
+  
+  play :E6
+  sleep 0.25
+  play :A5
+  sleep 0.25
+  play :F5
+  sleep 0.25
+  play :D5
+  play :E4, sustain: 0.5
+  sleep 0.25
+  
+  play :D6
+  sleep 0.25
+  play :A5
+  sleep 0.25
+  play :F5
+  sleep 0.25
+  play :D5
+  sleep 0.25
+  
+  play :Cs6
+  sleep 0.25
+  play :A5
+  sleep 0.25
+  play :F5
+  sleep 0.25
+  play :D5
+  sleep 0.25
+  
+  play :D6
+  sleep 0.25
+  play :A5
+  sleep 0.25
+  play :F5
+  sleep 0.25
+  play :D5
+  sleep 0.25
+  
+  
+  
+  
+  play :A6
+  sleep 0.25
+  play :D6
+  sleep 0.25
+  play :Bb5
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  
+  play :G6
+  sleep 0.25
+  play :D6
+  sleep 0.25
+  play :Bb5
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  
+  play :Fs6
+  sleep 0.25
+  play :D6
+  sleep 0.25
+  play :Bb5
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  
+  play :G6
+  sleep 0.25
+  play :D6
+  sleep 0.25
+  play :Bb5
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  
+  
+  play :Bb6
+  sleep 0.25
+  play :Eb6
+  sleep 0.25
+  play :C6
+  sleep 0.25
+  play :A5
+  sleep 0.25
+  
+  play :A6
+  sleep 0.25
+  play :Eb6
+  sleep 0.25
+  play :C6
+  sleep 0.25
+  play :A5
+  sleep 0.25
+  
+  play :Gs6
+  sleep 0.25
+  play :Eb6
+  sleep 0.25
+  play :C6
+  sleep 0.25
+  play :A5
+  sleep 0.25
+  
+  play :A6
+  sleep 0.25
+  play :Eb6
+  sleep 0.25
+  play :C6
+  sleep 0.25
+  play :A5
+  sleep 0.25
+  
+  
+  play :C7
+  sleep 0.25
+  play :D6
+  sleep 0.25
+  play :Bb5
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  
+  play :Bb6
+  sleep 0.25
+  play :D6
+  sleep 0.25
+  play :Bb5
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  
+  play :A6
+  sleep 0.25
+  play :D6
+  sleep 0.25
+  play :Bb5
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  
+  play :Bb6
+  sleep 0.25
+  play :D6
+  sleep 0.25
+  play :Bb5
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  
+  
+  play :A6
+  sleep 0.25
+  play :C6
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  play :E5
+  sleep 0.25
+  
+  play :G6
+  sleep 0.25
+  play :C6
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  play :E5
+  sleep 0.25
+  
+  play :F6
+  sleep 0.25
+  play :C6
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  play :E5
+  sleep 0.25
+  
+  play :E6
+  sleep 0.25
+  play :C6
+  sleep 0.25
+  play :G5
+  sleep 0.25
+  play :E5
+  sleep 0.25
+  
+end
