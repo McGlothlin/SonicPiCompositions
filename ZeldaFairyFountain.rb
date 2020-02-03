f458f17bb4bdc9d87a2c88fd99fab779049857e7
Sean McGlothlin <sean.michael.mcglothlin@gmail.com>
2020-02-03 05:35:39 +0000

    fixed sound levels

--- ZeldaFairyFountain.rb
+++ ZeldaFairyFountain.rb
@@ -1,466 +1,455 @@
 define :zelda_melody do
-  
-  play :A6
-  sleep 0.25
-  play :D6
-  play :Bb3
-  sleep 0.25
-  play :Bb5
-  play :D4
-  sleep 0.25
-  play :G5
-  play :A4, sustain: 0.5, amp: 0.7
-  sleep 0.25
-  
-  play :G6
-  sleep 0.25
-  play :D6
-  sleep 0.25
-  play :Bb5
-  sleep 0.25
-  play :G5
-  sleep 0.25
-  
-  play :Fs6
-  sleep 0.25
-  play :D6
-  play :D4
-  sleep 0.25
-  play :Bb5
-  play :A4
-  sleep 0.25
-  play :G5
-  play :G4
-  sleep 0.25
-  
-  play :G6
-  play :C5
-  sleep 0.25
-  play :D6
-  play :A4
-  sleep 0.25
-  play :Bb5
-  play :G4
-  sleep 0.25
-  play :G5
-  play :D4
-  sleep 0.25
-  
-  
-  play :G6
-  sleep 0.25
-  play :C6
-  play :A3
-  sleep 0.25
-  play :A5
-  play :C4
-  sleep 0.25
-  play :F5
-  play :G4, sustain: 0.5, amp: 0.7
-  sleep 0.25
-  
-  play :F6
-  sleep 0.25
-  play :C6
-  sleep 0.25
-  play :A5
-  sleep 0.25
-  play :F5
-  sleep 0.25
-  
-  play :E6
-  sleep 0.25
-  play :C6
-  play :C4
-  sleep 0.25
-  play :A5
-  play :G4
-  sleep 0.25
-  play :F5
-  play :F4
-  sleep 0.25
-  
-  play :F6
-  play :C5
-  sleep 0.25
-  play :C6
-  play :G4
-  sleep 0.25
-  play :A5
-  play :F4
-  sleep 0.25
-  play :F5
-  play :C4
-  sleep 0.25
-  
-  
-  play :F6
-  sleep 0.25
-  play :Bb5
-  play :G3
-  sleep 0.25
-  play :G5
-  play :Bb3
-  sleep 0.25
-  play :E5
-  play :F4, sustain: 0.5, amp: 0.7
-  sleep 0.25
-  
-  play :E6
-  sleep 0.25
-  play :Bb5
-  sleep 0.25
-  play :G5
-  sleep 0.25
-  play :E5
-  sleep 0.25
-  
-  play :Eb6
-  sleep 0.25
-  play :Bb5
-  play :Bb3
-  sleep 0.25
-  play :G5
-  play :F4
-  sleep 0.25
-  play :E5
-  play :E4
-  sleep 0.25
-  
-  play :E6
-  play :G4
-  sleep 0.25
-  play :Bb5
-  play :E4
-  sleep 0.25
-  play :G5
-  play :C4
-  sleep 0.25
-  play :E5
-  play :Bb3
-  sleep 0.25
-  
-  
-  play :E6
-  sleep 0.25
-  play :A5
-  play :F3
-  sleep 0.25
-  play :F5
-  play :A3
-  sleep 0.25
-  play :D5
-  play :E4, sustain: 0.5, amp: 0.7
-  sleep 0.25
-  
-  play :D6
-  sleep 0.25
-  play :A5
-  sleep 0.25
-  play :F5
-  sleep 0.25
-  play :D5
-  sleep 0.25
-  
-  play :Cs6
-  sleep 0.25
-  play :A5
-  play :C4
-  sleep 0.25
-  play :F5
-  play :E4
-  sleep 0.25
-  play :D5
-  play :D4
-  sleep 0.25
-  
-  play :D6
-  play :G4
-  sleep 0.25
-  play :A5
-  play :F4
-  sleep 0.25
-  play :F5
-  play :C5
-  sleep 0.25
-  play :D5
-  play :Bb4
-  sleep 0.25
-  
-  
-  
-  
-  play :A6
-  sleep 0.25
-  play :D6
-  play :Bb3
-  sleep 0.25
-  play :Bb5
-  play :D4
-  sleep 0.25
-  play :G5
-  play :A4, sustain: 0.5, amp: 0.7
-  sleep 0.25
-  
-  play :G6
-  sleep 0.25
-  play :D6
-  sleep 0.25
-  play :Bb5
-  sleep 0.25
-  play :G5
-  sleep 0.25
-  
-  play :Fs6
-  sleep 0.25
-  play :D6
-  play :D4
-  sleep 0.25
-  play :Bb5
-  play :A4
-  sleep 0.25
-  play :G5
-  play :G4
-  sleep 0.25
-  
-  play :G6
-  play :D5
-  sleep 0.25
-  play :D6
-  play :A4
-  sleep 0.25
-  play :Bb5
-  play :G4
-  sleep 0.25
-  play :G5
-  play :D4
-  sleep 0.25
-  
-  
-  play :Bb6
-  sleep 0.25
-  play :Eb6
-  play :Fs3
-  sleep 0.25
-  play :C6
-  play :C4
-  sleep 0.25
-  play :Fs5
-  play :C5, sustain: 0.5, amp: 0.7
-  sleep 0.25
-  
-  play :A6
-  sleep 0.25
-  play :Eb6
-  sleep 0.25
-  play :C6
-  sleep 0.25
-  play :Fs5
-  sleep 0.25
-  
-  play :Gs6
-  sleep 0.25
-  play :Eb6
-  play :D4
-  sleep 0.25
-  play :C6
-  play :C5
-  sleep 0.25
-  play :Fs5
-  play :A4
-  sleep 0.25
-  
-  play :A6
-  play :Eb5
-  sleep 0.25
-  play :Eb6
-  play :D5
-  sleep 0.25
-  play :C6
-  play :C5
-  sleep 0.25
-  play :Fs5
-  play :A4
-  sleep 0.25
-  
-  
-  play :C7
-  sleep 0.25
-  play :D6
-  play :Bb3
-  sleep 0.25
-  play :Bb5
-  play :D4
-  sleep 0.25
-  play :G5
-  play :Bb4
-  sleep 0.25
-  
-  play :Bb6
-  sleep 0.25
-  play :D6
-  sleep 0.25
-  play :Bb5
-  sleep 0.25
-  play :G5
-  sleep 0.25
-  
-  play :A6
-  sleep 0.25
-  play :D6
-  play :Bb3
-  sleep 0.25
-  play :Bb5
-  play :Bb4
-  sleep 0.25
-  play :G5
-  play :A4
-  sleep 0.25
-  
-  play :Bb6
-  play :C5
-  sleep 0.25
-  play :D6
-  play :Bb4
-  sleep 0.25
-  play :Bb5
-  play :G4
-  sleep 0.25
-  play :G5
-  play :D4
-  sleep 0.25
-  
-  
-  play :A6
-  sleep 0.25
-  play :C6
-  play :Bb3
-  sleep 0.25
-  play :G5
-  play :D4
-  sleep 0.25
-  play :E5
-  play :Bb4, sustain: 0.5, amp: 0.7
-  sleep 0.25
-  
-  play :G6
-  sleep 0.25
-  play :C6
-  sleep 0.25
-  play :G5
-  sleep 0.25
-  play :E5
-  sleep 0.25
-  
-  play :F6
-  sleep 0.25
-  play :C6
-  play :D4
-  sleep 0.25
-  play :G5
-  play :Bb4
-  sleep 0.25
-  play :E5
-  play :G4
-  sleep 0.25
-  
-  play :E6
-  play :E5
-  sleep 0.25
-  play :C6
-  play :D5
-  sleep 0.25
-  play :G5
-  play :G5
-  sleep 0.25
-  play :E5
-  play :E5
-  sleep 0.25
-  
+  with_fx :compressor, amp: 0.5 do
+    play :A6
+    sleep 0.25
+    play :D6
+    play :Bb3
+    sleep 0.25
+    play :Bb5
+    play :D4
+    sleep 0.25
+    play :G5
+    play :A4, sustain: 0.5, amp: 0.7
+    sleep 0.25
+    
+    play :G6
+    sleep 0.25
+    play :D6
+    sleep 0.25
+    play :Bb5
+    sleep 0.25
+    play :G5
+    sleep 0.25
+    
+    play :Fs6
+    sleep 0.25
+    play :D6
+    play :D4
+    sleep 0.25
+    play :Bb5
+    play :A4
+    sleep 0.25
+    play :G5
+    play :G4
+    sleep 0.25
+    
+    play :G6
+    play :C5
+    sleep 0.25
+    play :D6
+    play :A4
+    sleep 0.25
+    play :Bb5
+    play :G4
+    sleep 0.25
+    play :G5
+    play :D4
+    sleep 0.25
+    
+    
+    play :G6
+    sleep 0.25
+    play :C6
+    play :A3
+    sleep 0.25
+    play :A5
+    play :C4
+    sleep 0.25
+    play :F5
+    play :G4, sustain: 0.5, amp: 0.7
+    sleep 0.25
+    
+    play :F6
+    sleep 0.25
+    play :C6
+    sleep 0.25
+    play :A5
+    sleep 0.25
+    play :F5
+    sleep 0.25
+    
+    play :E6
+    sleep 0.25
+    play :C6
+    play :C4
+    sleep 0.25
+    play :A5
+    play :G4
+    sleep 0.25
+    play :F5
+    play :F4
+    sleep 0.25
+    
+    play :F6
+    play :C5
+    sleep 0.25
+    play :C6
+    play :G4
+    sleep 0.25
+    play :A5
+    play :F4
+    sleep 0.25
+    play :F5
+    play :C4
+    sleep 0.25
+    
+    
+    play :F6
+    sleep 0.25
+    play :Bb5
+    play :G3
+    sleep 0.25
+    play :G5
+    play :Bb3
+    sleep 0.25
+    play :E5
+    play :F4, sustain: 0.5, amp: 0.7
+    sleep 0.25
+    
+    play :E6
+    sleep 0.25
+    play :Bb5
+    sleep 0.25
+    play :G5
+    sleep 0.25
+    play :E5
+    sleep 0.25
+    
+    play :Eb6
+    sleep 0.25
+    play :Bb5
+    play :Bb3
+    sleep 0.25
+    play :G5
+    play :F4
+    sleep 0.25
+    play :E5
+    play :E4
+    sleep 0.25
+    
+    play :E6
+    play :G4
+    sleep 0.25
+    play :Bb5
+    play :E4
+    sleep 0.25
+    play :G5
+    play :C4
+    sleep 0.25
+    play :E5
+    play :Bb3
+    sleep 0.25
+    
+    
+    play :E6
+    sleep 0.25
+    play :A5
+    play :F3
+    sleep 0.25
+    play :F5
+    play :A3
+    sleep 0.25
+    play :D5
+    play :E4, sustain: 0.5, amp: 0.7
+    sleep 0.25
+    
+    play :D6
+    sleep 0.25
+    play :A5
+    sleep 0.25
+    play :F5
+    sleep 0.25
+    play :D5
+    sleep 0.25
+    
+    play :Cs6
+    sleep 0.25
+    play :A5
+    play :C4
+    sleep 0.25
+    play :F5
+    play :E4
+    sleep 0.25
+    play :D5
+    play :D4
+    sleep 0.25
+    
+    play :D6
+    play :G4
+    sleep 0.25
+    play :A5
+    play :F4
+    sleep 0.25
+    play :F5
+    play :C5
+    sleep 0.25
+    play :D5
+    play :Bb4
+    sleep 0.25
+    
+    
+    
+    play :A6
+    sleep 0.25
+    play :D6
+    play :Bb3
+    sleep 0.25
+    play :Bb5
+    play :D4
+    sleep 0.25
+    play :G5
+    play :A4, sustain: 0.5, amp: 0.7
+    sleep 0.25
+    
+    play :G6
+    sleep 0.25
+    play :D6
+    sleep 0.25
+    play :Bb5
+    sleep 0.25
+    play :G5
+    sleep 0.25
+    
+    play :Fs6
+    sleep 0.25
+    play :D6
+    play :D4
+    sleep 0.25
+    play :Bb5
+    play :A4
+    sleep 0.25
+    play :G5
+    play :G4
+    sleep 0.25
+    
+    play :G6
+    play :D5
+    sleep 0.25
+    play :D6
+    play :A4
+    sleep 0.25
+    play :Bb5
+    play :G4
+    sleep 0.25
+    play :G5
+    play :D4
+    sleep 0.25
+    
+    
+    play :Bb6
+    sleep 0.25
+    play :Eb6
+    play :Fs3
+    sleep 0.25
+    play :C6
+    play :C4
+    sleep 0.25
+    play :Fs5
+    play :C5, sustain: 0.5, amp: 0.7
+    sleep 0.25
+    
+    play :A6
+    sleep 0.25
+    play :Eb6
+    sleep 0.25
+    play :C6
+    sleep 0.25
+    play :Fs5
+    sleep 0.25
+    
+    play :Gs6
+    sleep 0.25
+    play :Eb6
+    play :D4
+    sleep 0.25
+    play :C6
+    play :C5
+    sleep 0.25
+    play :Fs5
+    play :A4
+    sleep 0.25
+    
+    play :A6
+    play :Eb5
+    sleep 0.25
+    play :Eb6
+    play :D5
+    sleep 0.25
+    play :C6
+    play :C5
+    sleep 0.25
+    play :Fs5
+    play :A4
+    sleep 0.25
+    
+    
+    play :C7
+    sleep 0.25
+    play :D6
+    play :Bb3
+    sleep 0.25
+    play :Bb5
+    play :D4
+    sleep 0.25
+    play :G5
+    play :Bb4
+    sleep 0.25
+    
+    play :Bb6
+    sleep 0.25
+    play :D6
+    sleep 0.25
+    play :Bb5
+    sleep 0.25
+    play :G5
+    sleep 0.25
+    
+    play :A6
+    sleep 0.25
+    play :D6
+    play :Bb3
+    sleep 0.25
+    play :Bb5
+    play :Bb4
+    sleep 0.25
+    play :G5
+    play :A4
+    sleep 0.25
+    
+    play :Bb6
+    play :C5
+    sleep 0.25
+    play :D6
+    play :Bb4
+    sleep 0.25
+    play :Bb5
+    play :G4
+    sleep 0.25
+    play :G5
+    play :D4
+    sleep 0.25
+    
+    
+    play :A6
+    sleep 0.25
+    play :C6
+    play :Bb3
+    sleep 0.25
+    play :G5
+    play :D4
+    sleep 0.25
+    play :E5
+    play :Bb4, sustain: 0.5, amp: 0.7
+    sleep 0.25
+    
+    play :G6
+    sleep 0.25
+    play :C6
+    sleep 0.25
+    play :G5
+    sleep 0.25
+    play :E5
+    sleep 0.25
+    
+    play :F6
+    sleep 0.25
+    play :C6
+    play :D4
+    sleep 0.25
+    play :G5
+    play :Bb4
+    sleep 0.25
+    play :E5
+    play :G4
+    sleep 0.25
+    
+    play :E6
+    play :E5
+    sleep 0.25
+    play :C6
+    play :D5
+    sleep 0.25
+    play :G5
+    sleep 0.25
+    play :E5
+    sleep 0.25
+  end
 end
 
 
 define :zelda_bass do
-  play :Bb1, sustain: 1.5, amp: 1.5
+  play :Bb1, sustain: 1.5
   sleep 2.5
-  play :Bb1, sustain: 0.5, amp: 1.5
+  play :Bb1, sustain: 0.5
   sleep 0.75
-  play :Bb1, sustain: 0.5, amp: 1.5
+  play :Bb1, sustain: 0.5
   sleep 0.25
-  play :Bb1, sustain: 0.5, amp: 1.5
+  play :Bb1, sustain: 0.5
   sleep 0.5
   
-  play :A1, sustain: 1.5, amp: 1.5
+  play :A1, sustain: 1.5
   sleep 2.5
-  play :A1, sustain: 0.5, amp: 1.5
+  play :A1, sustain: 0.5
   sleep 0.75
-  play :A1, sustain: 0.5, amp: 1.5
+  play :A1, sustain: 0.5
   sleep 0.25
-  play :A1, sustain: 0.5, amp: 1.5
-  sleep 0.5
-  play :A1, sustain: 0.5, amp: 1.5
-  
-  play :G1, sustain: 1.5, amp: 1.5
-  sleep 2.5
-  play :G1, sustain: 0.5, amp: 1.5
-  sleep 0.75
-  play :G1, sustain: 0.5, amp: 1.5
-  sleep 0.25
-  play :G1, sustain: 0.5, amp: 1.5
-  sleep 0.5
-  play :G1, sustain: 0.5, amp: 1.5
-  
-  play :F1, sustain: 1.5, amp: 1.5
-  sleep 2.5
-  play :F1, sustain: 0.5, amp: 1.5
-  sleep 0.75
-  play :F1, sustain: 0.5, amp: 1.5
-  sleep 0.25
-  play :F1, sustain: 0.5, amp: 1.5
-  sleep 0.5
-  play :F1, sustain: 0.5, amp: 1.5
-  
-  
-  play :Bb1, sustain: 1.5, amp: 1.5
-  sleep 2.5
-  play :Bb1, sustain: 0.5, amp: 1.5
-  sleep 0.75
-  play :Bb1, sustain: 0.5, amp: 1.5
-  sleep 0.25
-  play :Bb1, sustain: 0.5, amp: 1.5
+  play :A1, sustain: 0.5
   sleep 0.5
   
-  play :A1, sustain: 1.5, amp: 1.5
+  play :G1, sustain: 1.5
   sleep 2.5
-  play :A1, sustain: 0.5, amp: 1.5
+  play :G1, sustain: 0.5
   sleep 0.75
-  play :D2, sustain: 0.25, amp: 1.5
+  play :G1, sustain: 0.5
   sleep 0.25
-  play :D2, sustain: 0.1, amp: 1.5
+  play :G1, sustain: 0.5
   sleep 0.5
-  play :D2, sustain: 0.1, amp: 1.5
   
-  play :G1, sustain: 1.5, amp: 1.5
+  play :F1, sustain: 1.5
   sleep 2.5
-  play :G1, sustain: 0.5, amp: 1.5
+  play :F1, sustain: 0.5
   sleep 0.75
-  play :G1, sustain: 0.5, amp: 1.5
+  play :F1, sustain: 0.5
   sleep 0.25
-  play :G1, sustain: 0.5, amp: 1.5
+  play :F1, sustain: 0.5
   sleep 0.5
-  play :G1, sustain: 0.5, amp: 1.5
   
-  play :C2, sustain: 1.5, amp: 1.5
+  
+  play :Bb1, sustain: 1.5
   sleep 2.5
-  play :C2, sustain: 0.5, amp: 1.5
+  play :Bb1, sustain: 0.5
   sleep 0.75
-  play :C2, sustain: 0.5, amp: 1.5
+  play :Bb1, sustain: 0.5
   sleep 0.25
-  play :C2, sustain: 0.5, amp: 1.5
+  play :Bb1, sustain: 0.5
   sleep 0.5
-  play :C2, sustain: 0.5, amp: 1.5
   
+  play :A1, sustain: 1.5
+  sleep 2.5
+  play :A1, sustain: 0.5
+  sleep 0.75
+  play :D2, sustain: 0.25
+  sleep 0.25
+  play :D2, sustain: 0.1
+  sleep 0.5
   
+  play :G1, sustain: 1.5
+  sleep 2.5
+  play :G1, sustain: 0.5
+  sleep 0.75
+  play :G1, sustain: 0.5
+  sleep 0.25
+  play :G1, sustain: 0.5
+  sleep 0.5
+  
+  play :C2, sustain: 1.5
+  sleep 2.5
+  play :C2, sustain: 0.5
+  sleep 0.75
+  play :C2, sustain: 0.5
+  sleep 0.25
+  play :C2, sustain: 0.5
+  sleep 0.5
 end
 
 
@@ -468,7 +457,7 @@ define :kick do
   sample :bd_klub
   sleep 1
   with_fx :reverb, mix: 0.5, damp: 0.1, room: 0.8 do
-    sample  :perc_snap#, amp: 1.5
+    sample  :perc_snap, amp: 0.5
   end
   sleep 1.5
   sample :bd_klub
@@ -480,7 +469,7 @@ define :kick do
 end
 
 define :cymbal do
-  sample :drum_cymbal_closed, amp: 0.5
+  sample :drum_cymbal_closed, amp: 0.25
   sleep 0.25
 end
 
@@ -488,25 +477,35 @@ end
 
 
 use_bpm 78
-count = 0
 
-4.times do
+2.times do
   live_loop :cymbal do
     cymbal
   end
   kick
 end
 
-live_loop :trap do
-  kick
+in_thread do
+  live_loop :trap do
+    kick
+  end
 end
-live_loop :cymbal do
-  cymbal
+
+in_thread do
+  live_loop :cymbal do
+    cymbal
+  end
 end
-live_loop :zelda_main do
-  
-  zelda_melody
+
+in_thread do
+  live_loop :zelda_main do
+    zelda_melody
+  end
 end
-live_loop :zelda_rhythm do
-  zelda_bass
+
+in_thread do
+  live_loop :zelda_rhythm do
+    zelda_bass
+  end
 end
+
